073fac8731d020cc1994c12d00eaeef45774e6d6
Thomas Verderesi <tverderesi@gmail.com>
2022-12-01 02:49:00 -0300

    added tower availiability

--- src/components/routes/GenericRoute.tsx
+++ src/components/routes/GenericRoute.tsx
@@ -1,6 +1,6 @@
 import FloorGenerator from '../widgets/FloorGenerator';
 import BuildingLayout from '../layout/BuildingLayout';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import AppContext from '../../context/AppContext';
 import Grid from '../widgets/Grid';
 import Icon from '../widgets/BuildingOccupancy';
@@ -23,7 +23,21 @@ export default function GenericRoute({ param }: { param: any }) {
   return item.building ? <Building item={item} /> : <p>Hello Worlde</p>;
 }
 
+const b = {
+  'total': 90,
+  'available': 42,
+  'negotiation': 23,
+  'unavailable': 25,
+};
+
+const a = {
+  'total': 100,
+  'available': 33,
+  'negotiation': 33,
+  'unavailable': 33,
+};
 export function Building({ item }: { item: any }) {
+  const [tower, setTower] = useState(1);
   return (
     <BuildingLayout>
       <div className='flex flex-col items-center min-w-[100vw] justify-start min-h-[calc(100vh-6rem)] 0 lg:mt-10'>
@@ -57,7 +71,7 @@ export function Building({ item }: { item: any }) {
           >
             <div className='flex flex-col justify-between h-[31.5vh]'>
               <div>
-                <p className=' text-md lg:text-xl my-2 text-cadetblue-300 uppercase font-normal tracking-wider'>
+                <p className=' text-md lg:text-xl my-2 text-cadetblue-300 uppercase font-semibold tracking-wider'>
                   Previsão de Entrega
                 </p>
                 <p className='text-lg lg:text-xl text-white  font-medium tracking-wider'>
@@ -75,42 +89,89 @@ export function Building({ item }: { item: any }) {
               </div>
             </div>
           </div>
-          <div className='divider mt-0 lg:divider-horizontal'></div>
+          <div className='divider mt-0 mx-0 lg:mx-0 lg:divider-horizontal'></div>
           <div className='w-full lg:w-1/2 h-auto lg:h-full'>
             <div className='flex flex-col w-full px-2'>
-              <div className=' w-full text-left  h-full font-normal text-lg lg:text-2xl uppercase tracking-wide  text-cadetblue-300'>
-                Unidades
-              </div>
-              <div className='flex lg:ml-[-16px] lg:mt-4 h-full my-auto  items-center  text-medium tracking-wide'>
-                <Icon />
-                <div className='w-4/5 ml-3 flex flex-col justify-center h-full'>
-                  <p className='text-green-500 text-md font-light lg:text-xl mt-2 lg:mt-4'>
-                    <span className='lg:text-3xl font-black'>33%</span> Em
-                    Negociação
-                  </p>
-                  <p className='text-green-500 text-md font-light lg:text-xl  lg:mb-4'>
-                    <span className='lg:text-2xl font-black'>XX</span> Unidades
-                  </p>
-                  <p className='text-yellow-500 text-md font-light lg:text-xl mt-2 lg:mt-4'>
-                    <span className='lg:text-3xl font-black'>33%</span> Em
-                    Negociação
-                  </p>
-                  <p className='text-yellow-500 text-md font-light lg:text-xl lg:mb-4'>
-                    <span className='lg:text-2xl font-black'>XX</span> Unidades
-                  </p>
-                  <p className='text-red-500 text-md font-light lg:text-xl mt-2 lg:mt-4'>
-                    <span className='lg:text-3xl font-black'>33%</span>{' '}
-                    Indisponíveis
-                  </p>
-                  <p className='text-red-500 text-md font-light lg:text-xl lg:mb-4'>
-                    <span className='lg:text-2xl font-black'>XX</span> Unidades
-                  </p>
+              <div className='flex items-center justify-start flex-wrap '>
+                <div className=' w-[98vw] lg:w-auto text-left  h-full font-semibold text-lg lg:text-2xl uppercase tracking-wide  text-cadetblue-300'>
+                  Unidades
                 </div>
+                <button
+                  className='btn btn-ghost lg:ml-3 text-cadetblue-400 text-md lg:text-lg font-medium tracking-wide uppercase'
+                  onClick={() => {
+                    setTower(1);
+                  }}
+                >
+                  Torre A
+                </button>
+                <button
+                  className='btn btn-ghost lg:ml-3 text-cadetblue-400 text-md lg:text-lg font-medium tracking-wide uppercase'
+                  onClick={() => {
+                    setTower(2);
+                  }}
+                >
+                  Torre B
+                </button>
               </div>
+
+              {tower === 1 ? (
+                <Availability availability={a} />
+              ) : (
+                <Availability availability={b} />
+              )}
             </div>
           </div>
         </section>
       </div>
     </BuildingLayout>
+  );
+}
+function Availability({ availability }: { availability: any }) {
+  const available = Math.round(
+    (availability.available / availability.total) * 100
+  );
+  const unavailable = Math.round(
+    (availability.unavailable / availability.total) * 100
+  );
+  const negotiation = Math.round(
+    (availability.negotiation / availability.total) * 100
+  );
+
+  return (
+    <div className='flex lg:ml-[-16px] lg:mt-4 h-full my-auto  items-center  text-medium tracking-wide'>
+      <Icon />
+      <div className='w-4/5 ml-3 flex flex-col justify-center h-full'>
+        <p className='text-green-500 text-md font-light lg:text-xl mt-2 lg:mt-4'>
+          <span className='lg:text-3xl font-black'>{available}%</span> Em
+          Negociação
+        </p>
+        <p className='text-green-500 text-md font-light lg:text-xl  lg:mb-4'>
+          <span className='lg:text-2xl font-black'>
+            {availability.available}
+          </span>{' '}
+          Unidades
+        </p>
+        <p className='text-yellow-500 text-md font-light lg:text-xl mt-2 lg:mt-4'>
+          <span className='lg:text-3xl font-black'>{negotiation}%</span> Em
+          Negociação
+        </p>
+        <p className='text-yellow-500 text-md font-light lg:text-xl lg:mb-4'>
+          <span className='lg:text-2xl font-black'>
+            {availability.negotiation}
+          </span>{' '}
+          Unidades
+        </p>
+        <p className='text-red-500 text-md font-light lg:text-xl mt-2 lg:mt-4'>
+          <span className='lg:text-3xl font-black'>{unavailable}%</span>{' '}
+          Indisponíveis
+        </p>
+        <p className='text-red-500 text-md font-light lg:text-xl lg:mb-4'>
+          <span className='lg:text-2xl font-black'>
+            {availability.unavailable}
+          </span>{' '}
+          Unidades
+        </p>
+      </div>
+    </div>
   );
 }
