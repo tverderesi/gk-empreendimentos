f415bace51e5eec75379985681c35af059e447d0
tverderesi <tverderesi@gmail.com>
2023-04-14 14:22:35 -0300

    renamed carousel to reflect its functionality

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,8 @@
-import { SwipeCarousel } from "../components/widgets/SwipeCarousel";
+import { ResponsiveCarousel } from "../components/widgets/ResponsiveCarousel";
 
 export default function Home() {
   return (
-    <SwipeCarousel
+    <ResponsiveCarousel
       className="h-[calc(100vh-22rem)] md:h-[calc(100vh-12rem)] transition-all duration-500 ease-in-out"
       images={[
         { src: "./pages/about/01-1024x576.jpg", alt: "test" },
