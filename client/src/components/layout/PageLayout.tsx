16f60a59605e0c01111ea9026a743503119038af
tverderesi <tverderesi@gmail.com>
2023-04-14 14:21:05 -0300

    updated page layout to be more generic

--- client/src/components/layout/PageLayout.tsx
+++ client/src/components/layout/PageLayout.tsx
@@ -2,22 +2,16 @@ import { Outlet } from "react-router-dom";
 import { BackgroundImage } from "../atoms/BackgroundImage";
 
 import Navbar from "../widgets/Navbar";
+import { Footer } from "../atoms/Footer";
 
 export function PageLayout() {
   return (
     <div className="pt-24 min-h-screen">
       <Navbar />
-      <main className="relative overflow-x-hidden overflow-y-scroll w-screen min-h-[calc(100vh-12rem)]">
+      <main className="relative overflow-x-hidden overflow-y-scroll w-screen min-h-[calc(100vh-22rem)] md:min-h-[calc(100vh-12rem)]">
         <Outlet />
       </main>
-      <footer className="custom-footer">
-        <p className="mx-auto w-full">
-          Av. Vereador Manoel José dos Santos, 1.310, Sala 14, Bombinhas - SC
-          <span className="block sm:inline">
-            gkengenharia@gmail.com (47)3264-0018
-          </span>
-        </p>
-      </footer>
+      <Footer />
       <BackgroundImage />
     </div>
   );
