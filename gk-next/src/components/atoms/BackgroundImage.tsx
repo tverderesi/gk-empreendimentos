bba1878f9549b76023cd86d85f8e77415606bea4
tverderesi <tverderesi@gmail.com>
2023-05-22 11:02:19 -0300

    add pages

--- gk-next/src/components/atoms/BackgroundImage.tsx
+++ gk-next/src/components/atoms/BackgroundImage.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
+
 export function BackgroundImage({ className = "" }: { className?: string }) {
   return (
-    <img
-      src={`${process.env.PUBLIC_URL}/media/lumina_overlay.png`}
+    <Image
+      src={`/media/lumina_overlay.png`}
       className={`object-cover opacity-20 brightness-[30%] absolute bottom-0 h-[50vh] left-0 -z-10 ${className}`}
       alt="Edifício Lumina"
+      fill
     />
   );
 }
